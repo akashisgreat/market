a4748aa9412fbacbc7a373627bfe5b91288a54c7
Akash <akashisgreat0000@gmail.com>
2023-12-08 15:52:19 +0530

    Initial Commit

---
+++ data.js
@@ -0,0 +1,18 @@
+DATA = [
+    {
+        cname: "NIPPON INDIA MUTUAL FUND SMALL CAP FUND GROWTH PL - GROWTH OPEN ENDED",
+        isin: "INF204K01HY3"
+    },
+    {
+        cname: "QUANT MUTUAL FUND SMALL CAP FUND DIR GROWTH OPEN",
+        isin: "INF966L01689"
+    },
+    {
+        cname: "HDFC Small Cap Fund - Direct Plan - Growth",
+        isin: "INF179KA1RW5"
+    },
+    {
+        cname: "SBI Small Cap Fund - Regular Plan - Growth",
+        isin: "INF200K01T28"
+    }
+]
