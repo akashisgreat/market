25085c2c427a2651a87b68c0788eb7dc2d871f84
Akash <akashisgreat0000@gmail.com>
2024-02-01 22:57:09 +0530

    removed nav-price.html,nav-price-history.html and scripts/get-cname-with-isin-no.sh scripts/get-isin-no-and-cname-with-url.sh data.js &&&&& added interest calculator

--- root/header.js
+++ root/header.js
@@ -1,8 +1,10 @@
-const headerHTML = ` <header> <nav>
-<a href="index.html">Future Market</a> &nbsp;|&nbsp; <a href="nav-price.html">NAV Price</a> &nbsp;|&nbsp; <a
-    href="calculator.html">Calculator</a>
-</nav></header>`
+const headerHTML = `
+<header>
+    <nav>
+        <a href="index.html">Future Market</a> &nbsp;|&nbsp; 
+        <a href="calculator.html">Calculator</a>
+    </nav>
+</header>`
 
 header = document.querySelector('header')
-
 header.outerHTML = headerHTML
