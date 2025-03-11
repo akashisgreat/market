33963a63e8ca489749a3736760a1313b2141d9e2
Akash <akashisgreat0000@gmail.com>
2025-03-12 03:15:03 +0530

    added land-area-calc.html file

--- root/header.js
+++ root/header.js
@@ -3,6 +3,7 @@ const headerHTML = `
     <nav>
         <a href="index.html">Future Market</a> &nbsp;|&nbsp; 
         <a href="calculator.html">Calculator</a>
+        <a href="land-area-calc.html">Land Area Calc</a>
     </nav>
 </header>`
 
