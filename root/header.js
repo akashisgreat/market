1f2ddcc7f88407dfcd5e789e7890db000a263f92
Akash <akashisgreat0000@gmail.com>
2025-03-12 03:20:29 +0530

    fix litle ui

--- root/header.js
+++ root/header.js
@@ -1,8 +1,10 @@
 const headerHTML = `
 <header>
     <nav>
-        <a href="index.html">Future Market</a> &nbsp;|&nbsp; 
+        <a href="index.html">Future Market</a> 
+        &nbsp;|&nbsp; 
         <a href="calculator.html">Calculator</a>
+        &nbsp;|&nbsp; 
         <a href="land-area-calc.html">Land Area Calc</a>
     </nav>
 </header>`
